96e1085833919491e718120880f1b9b0d91f6be7
Umid221 <najimovumid2200@gmail.com>
2023-08-26 18:21:24 +0500

    routes moved to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import Layout from "src/components/layout";
-import Home from "src/pages/Home";
-import Signup from "src/pages/Signup";
-import Login from "src/pages/Login";
 import theme from "src/styles/theme";
-
-const router = createBrowserRouter([
-    {
-        path: "",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-        ],
-    },
-    {
-        path: "/register",
-        element: <Signup />,
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-]);
+import router from "./routes";
 
 function App() {
     return (
---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import Layout from "src/components/layout";
+import Home from "src/pages/Home";
+import Signup from "src/pages/Signup";
+import Login from "src/pages/Login";
+import { createBrowserRouter } from "react-router-dom";
+// import React from "react";
+// const Login = React.lazy(() => import("./pages/Login"));
+
+const router = createBrowserRouter([
+    {
+        path: "",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+    {
+        path: "/register",
+        element: <Signup />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+]);
+
+export default router;
